3df0f9c2dfe61d9698ec82ba823bc9bfeea19412
ItsLaro <ivan.reor@gmail.com>
2020-10-17 10:26:33 -0400

    Created login page from template

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import home from "./pages/home"
 import dashboard from "./pages/dashboard"
 import insights from "./pages/insights"
 import order from "./pages/order"
+import login from "./pages/login"
 
 function App() {
   return (
     <>
-      
       <Switch>
         <Route path="/" component={home} exact />
         <Route path="/dashboard" component={dashboard} />
         <Route path="/insights" component={insights} />
         <Route path="/order" component={order} />
+        <Route path="/login" component={login} />
       </Switch>
     </>
   );

59c45cd52c311f534db178ac8d48e1db75a815bd
raahimid <raahim.idrees@gmail.com>
2020-10-17 04:25:52 -0400

    Added sidebar, needs CSS

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Nav from '../components/Nav'
+import React from "react";
+import Nav from "../components/Nav";
+import Sidebar from "../components/Sidebar";
 
 export default function dashboard() {
   return (
     <>
       <Nav />
+      <Sidebar />
       <h1>Dashboard Page</h1>
     </>
-  )
+  );
 }

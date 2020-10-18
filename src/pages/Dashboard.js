828a15fbdc094ef28a7d5f8bd7b3696b54f548ff
ItsLaro <ivan.reor@gmail.com>
2020-10-18 05:47:21 -0400

    Minor UI fixes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,15 @@
 
 import React from "react";
 import Layout from "../components/Layout";
-
+import { Container, Grid } from "@material-ui/core";
 import LocalShippingIcon from '@material-ui/icons/LocalShipping';
 
 export default function Dashboard() {
   return (
     <Layout shouldShowHeader={false}>
-        <h1>Hello, Name</h1>
+          <Container maxWidth={false} className="mt-5">
+          <h1>Hello, Name</h1>
+        </Container>
     </Layout>
   );
 }

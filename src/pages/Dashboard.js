c85b5e7f5e498ef8d5ff73dd9895a76a4447fcbc
ItsLaro <ivan.reor@gmail.com>
2020-10-18 06:38:25 -0400

    Changes to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,140 @@
 
 import React from "react";
 import Layout from "../components/Layout";
-import { Container, Grid } from "@material-ui/core";
-import LocalShippingIcon from '@material-ui/icons/LocalShipping';
+import {Container} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Block } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  button: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function getSteps() {
+  return ['Connect to Your Store', 'Define and Implement Your Logistics', 'Connect with Your Clients'];
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return 'Connect to Your Store';
+    case 1:
+      return 'Define and Implement Your Logistics';
+    case 2:
+      return 'Connect with Your Clients';
+    default:
+      return 'Unknown step';
+  }
+}
 
 export default function Dashboard() {
+
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+  const steps = getSteps();
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
   return (
     <Layout shouldShowHeader={false}>
-          <Container maxWidth={false} className="mt-5">
-          <h1>Hello, Name</h1>
+
+      <div>
+        <Container maxWidth={true} className="mt-5">
+          <div className="container">
+            <h1 style={{color: "#000000"}}>Hello, Name</h1>
+            <br/>
+            <h2 className="mb-3" style={{color: "#000000"}}>Get Started</h2>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+              if (isStepSkipped(index)) {
+                stepProps.completed = false;
+              }
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+            <div>
+              {activeStep === steps.length ? (
+                <div>
+                  <Typography className={classes.instructions}>
+                    All steps completed - you&apos;re finished
+                  </Typography>
+                  <Button onClick={handleReset} className={classes.button}>
+                    Reset
+                  </Button>
+                </div>
+              ) : (
+                <div>
+                  <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+                  <div>
+                    <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+                      Back
+                    </Button>
+
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </Container>
+      </div>
     </Layout>
   );
 }
